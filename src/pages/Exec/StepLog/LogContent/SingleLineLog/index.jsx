ce65d9bea1d64803fa04e0daaadbf1da7a5578ee
lovixk <991807272@qq.com>
2022-04-01 10:17:57 +0800

    Tomato-Scrum-30359 [Task]  修复日志重复问题

--- src/pages/Exec/StepLog/LogContent/SingleLineLog/index.jsx
+++ src/pages/Exec/StepLog/LogContent/SingleLineLog/index.jsx
@@ -1,6 +1,6 @@
 import cx from './index.less';
 const SingleLineLog = ({content}) => {
-    return <div className={cx('single-line-log')}>{content}{content}{content}</div>;
+    return <div className={cx('single-line-log')}>{content}</div>;
 };
 
 export default SingleLineLog;
